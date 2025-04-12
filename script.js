ad34f82d56ebdbbf28d3e5181c81c10c597d3e4e
AdrishikharChowdhury <amiadrishikhar@gmail.com>
2025-04-12 10:26:16 +0530

    Initial commit

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ if (window.location.hostname === "adrishikharchowdhury.github.io") {
     baseTag.href = "/Birthday/"; // Adjust to your project name
     document.head.appendChild(baseTag);
 }
+
+const heading = document.querySelector(".heading");
+const audio = new Audio("audio.mp3"); // Make sure the file exists in the right place
+
+let isPlaying = false;
+
+heading.addEventListener("click", () => {
+    if (isPlaying) {
+        audio.pause();
+    } else {
+        audio.play();
+    }
+    isPlaying = !isPlaying;
+});
