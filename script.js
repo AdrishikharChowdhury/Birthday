828fae4e02b6331cabe2e5fdd317fbdd87c73e60
AdrishikharChowdhury <amiadrishikhar@gmail.com>
2025-04-12 09:58:33 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+if (window.location.hostname === "adrishikharchowdhury.github.io") {
+    const baseTag = document.createElement("base");
+    baseTag.href = "/Birthday/"; // Adjust to your project name
+    document.head.appendChild(baseTag);
+}
